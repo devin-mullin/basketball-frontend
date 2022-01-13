566a1ad4c58270537732521a6723ed45ec068312
Devin Mullin <devin.mullin90@gmail.com>
2022-01-13 14:55:43 -0600

    game 1 matchup stats done

--- src/components/RecentGames.jsx
+++ src/components/RecentGames.jsx
@@ -7,9 +7,10 @@ function RecentGames({ games }){
 
 let navigate = useNavigate()
 
-// stats selectors
+// GAME 1 stats selectors
 const players = useSelector(selectPlayers)
 //ppg
+const ppg = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[0]?.shortDisplayName
 const ppgPlayer1 = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[0]?.leaders[0]?.athlete?.displayName
 const thisPlayer1 = players[0]?.find(player=> player.name === ppgPlayer1)
 const thisPlayer1pic = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[0]?.leaders[0]?.athlete?.headshot
@@ -17,13 +18,23 @@ const ppgPlayer2 = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[0]
 const thisPlayer2 = players[0]?.find(player=> player.name === ppgPlayer2)
 const thisPlayer2pic = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[0]?.leaders[0]?.athlete?.headshot
 // rpg
+const rpg = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[1]?.shortDisplayName
 const rpgPlayer1 = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[1]?.leaders[0]?.athlete?.displayName
 const findRpgPlayer1 = players[0]?.find(player=> player.name === rpgPlayer1)
 const rpgPlayer1pic = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[1]?.leaders[0]?.athlete?.headshot
 const rpgPlayer2 = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[1]?.leaders[0]?.athlete?.displayName
 const findRpgPlayer2 = players[0]?.find(player=> player.name === rpgPlayer2)
-console.log(rpgPlayer2);
 const rpgPlayer2pic = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[1]?.leaders[0]?.athlete?.headshot
+//apg
+const apg = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[2]?.shortDisplayName
+const apgPlayer1 = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[2]?.leaders[0]?.athlete?.displayName
+const findApgPlayer1 = players[0]?.find(player=> player.name === rpgPlayer1)
+const apgPlayer1pic = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[2]?.leaders[0]?.athlete?.headshot
+const apgPlayer1Stat = games?.events[0]?.competitions[0]?.competitors[0]?.leaders[2]?.leaders[0]?.displayValue
+const apgPlayer2 = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[2]?.leaders[0]?.athlete?.displayName
+const findApgPlayer2 = players[0]?.find(player=> player.name === rpgPlayer2)
+const apgPlayer2pic = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[2]?.leaders[0]?.athlete?.headshot
+const apgPlayer2Stat = games?.events[0]?.competitions[0]?.competitors[1]?.leaders[2]?.leaders[0]?.displayValue
 
 
 
@@ -111,9 +122,83 @@ const rpgPlayer2pic = games?.events[0]?.competitions[0]?.competitors[1]?.leaders
                         {rpgPlayer1}
                     </strong>
             </Link>
-            <span> | </span>{games?.events[0]?.competitions[0]?.competitors[1]?.leaders[1]?.leaders[0]?.displayValue} 
+            <span> | </span>{games?.events[0]?.competitions[0]?.competitors[0]?.leaders[1]?.leaders[0]?.displayValue} 
             <br/>
             <br/>
+    {games?.events[0]?.competitions[0]?.competitors[1]?.team?.shortDisplayName}
+        
+        <br/>
+    <img 
+        className="player-boxscore-pic" 
+        src={rpgPlayer2pic}
+        onClick={()=> navigate(`/players/${findRpgPlayer2.id}`)} 
+        style={{cursor: 'pointer'}}
+        />
+    <br/>
+    <Link 
+            className="player-boxscore-link"
+            to=
+          {{pathname: `/players/${findRpgPlayer2.id}`}} 
+           
+           >
+            <strong>
+                {rpgPlayer2}
+            </strong>
+    </Link>
+    <span> | </span>{games?.events[0]?.competitions[0]?.competitors[1]?.leaders[1]?.leaders[0]?.displayValue} 
+    <br/>
+    <br/>
+    <strong>
+        {apg}
+        </strong>
+                <br/>
+                <br/>
+            {games?.events[0]?.competitions[0]?.competitors[0]?.team?.shortDisplayName}
+        
+                <br/>
+            <img 
+                className="player-boxscore-pic" 
+                src={apgPlayer1pic}
+                onClick={()=> navigate(`/players/${findApgPlayer1.id}`)} 
+                style={{cursor: 'pointer'}}
+                />
+            <br/>
+            <Link 
+                    className="player-boxscore-link"
+                    to=
+                  {{pathname: `/players/${findApgPlayer1.id}`}} 
+                   
+                   >
+                    <strong>
+                        {apgPlayer1}
+                    </strong>
+            </Link>
+            <span> | </span>{apgPlayer1Stat} 
+            <br/>
+            <br/>
+    {games?.events[0]?.competitions[0]?.competitors[1]?.team?.shortDisplayName}
+        
+        <br/>
+    <img 
+        className="player-boxscore-pic" 
+        src={apgPlayer2pic}
+        onClick={()=> navigate(`/players/${findApgPlayer2.id}`)} 
+        style={{cursor: 'pointer'}}
+        />
+    <br/>
+    <Link 
+            className="player-boxscore-link"
+            to=
+          {{pathname: `/players/${findApgPlayer2.id}`}} 
+           
+           >
+            <strong>
+                {apgPlayer2}
+            </strong>
+    </Link>
+    <span> | </span>{apgPlayer2Stat} 
+    <br/>
+    <br/>
        
         </div>
     </container>
