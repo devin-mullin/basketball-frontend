ddede09daf1707b46ad7cc8fb6a699c92b4cca74
Devin Mullin <devin.mullin90@gmail.com>
2022-02-10 12:06:16 -0600

    changed margins on search and login / ui improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
@@ -14,9 +13,3 @@ import { Provider } from 'react-redux';
       </Provider>,
       document.getElementById('root'),
     )
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
