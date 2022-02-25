f40152d777a0ead918ed0acb93f390a5972d2f5a
Devin Mullin <devin.mullin90@gmail.com>
2022-02-25 14:41:52 -0600

    mobile responsiveness for teams page and team page

--- src/components/NbaTeam.jsx
+++ src/components/NbaTeam.jsx
@@ -1,89 +1,112 @@
-import { selectAllTeams } from './redux/teamSlice'
-import { addPlayer } from './redux/myTeamSlice'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams, Link, useNavigate } from 'react-router-dom'
-import { Table } from 'react-bootstrap'
+import { selectAllTeams } from "./redux/teamSlice";
+import { addPlayer } from "./redux/myTeamSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { Table } from "react-bootstrap";
 
+function NbaTeam({ selectedTeam }) {
+  const dispatch = useDispatch();
 
-function NbaTeam({ selectedTeam }){
-  const dispatch = useDispatch()
+  const teams = useSelector(selectAllTeams);
+  const pgNum = useParams();
+  const id = parseInt(pgNum.id);
+  const myTeam = teams[0]?.find((team) => team.id === id);
+  const navigate = useNavigate();
 
-  
-  const teams = useSelector(selectAllTeams) 
-  const pgNum = useParams()
-  const id = parseInt(pgNum.id)
-  const myTeam = teams[0]?.find(team=>team.id === id)
-  const navigate = useNavigate()
-
-  const handleClick = (e) =>{
-    alert(`${e.target.name} added to ${selectedTeam.name}!`)
-    const player = myTeam.players.filter(player=>player.id === e.target.id)
+  const handleClick = (e) => {
+    alert(`${e.target.name} added to ${selectedTeam.name}!`);
+    const player = myTeam.players.filter((player) => player.id === e.target.id);
     console.log(player);
-    fetch('https://nameless-ravine-11360.herokuapp.com/user_team_players', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            user_team_id: selectedTeam.id,
-            player_id: e.target.id
-        }),
+    fetch("https://nameless-ravine-11360.herokuapp.com/user_team_players", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_team_id: selectedTeam.id,
+        player_id: e.target.id,
+      }),
     })
-        .then(response => response.json())
-        .then(()=>dispatch(addPlayer(player)))
-        .catch((error) => {
-        console.error('Error:', error);
-        });
-            }
+      .then((response) => response.json())
+      .then(() => dispatch(addPlayer(player)))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-
-    return(
-        <div className="team">
-              <div className="back-button-div">
-                  <button className="back-button" 
-                          onClick={()=>navigate(-1)}>
-                            back
-                  </button>
-              </div>
-          <h3>{myTeam?.full_name}</h3>
-         <Table striped bordered hover 
-         className="table" size="sm"
-        
-         >
-           <thead>
+  return (
+    <div className="team">
+      <div className="back-button-div">
+        <button className="back-button" onClick={() => navigate(-1)}>
+          back
+        </button>
+      </div>
+      <br />
+      <br />
+      <br />
+      <h3>{myTeam?.full_name}</h3>
+      <Table striped bordered hover className="table" size="sm">
+        <thead>
           <tr>
-          <th><strong>Name</strong></th>
-              <th><strong>AGE</strong></th>
-              <th><strong>POS</strong></th>
-              <th><strong>GP</strong></th>
-              <th><strong>GS</strong></th>
-              <th><strong>PPG</strong></th>
-              <th><strong>FG%</strong></th>
-              <th><strong>3P%</strong></th>
-              <th><strong>FT%</strong></th>
-              <th><strong>MPG</strong></th>
-              <th><strong>RPG</strong></th>
-              <th><strong>APG</strong></th>
-              <th><strong>SPG</strong></th>
-              <th><strong>BPG</strong></th>
-              <th><strong>TOV</strong></th>
-              <th></th>
+            <th>
+              <strong>Name</strong>
+            </th>
+            <th>
+              <strong>AGE</strong>
+            </th>
+            <th>
+              <strong>POS</strong>
+            </th>
+            <th>
+              <strong>GP</strong>
+            </th>
+            <th>
+              <strong>GS</strong>
+            </th>
+            <th>
+              <strong>PPG</strong>
+            </th>
+            <th>
+              <strong>FG%</strong>
+            </th>
+            <th>
+              <strong>3P%</strong>
+            </th>
+            <th>
+              <strong>FT%</strong>
+            </th>
+            <th>
+              <strong>MPG</strong>
+            </th>
+            <th>
+              <strong>RPG</strong>
+            </th>
+            <th>
+              <strong>APG</strong>
+            </th>
+            <th>
+              <strong>SPG</strong>
+            </th>
+            <th>
+              <strong>BPG</strong>
+            </th>
+            <th>
+              <strong>TOV</strong>
+            </th>
+            <th></th>
           </tr>
-          </thead>
-          <tbody>
-          {
-            myTeam?.players.map((player)=>(
-          <tr key={player.id}>
+        </thead>
+        <tbody>
+          {myTeam?.players.map((player) => (
+            <tr key={player.id}>
               <td>
                 <p
-                    className="player-link"
-                    onClick={()=> navigate(`/players/${player.id}`)} 
-                   >
-                    <strong>
-                      {player?.name}
-                    </strong>
+                  className="player-link"
+                  onClick={() => navigate(`/players/${player.id}`)}
+                >
+                  <strong>{player?.name}</strong>
                 </p>
-                </td>
+              </td>
               <td>{player?.age}</td>
               <td>{player?.pos}</td>
               <td>{player?.g}</td>
@@ -99,22 +122,20 @@ function NbaTeam({ selectedTeam }){
               <td>{player?.blk}</td>
               <td>{player?.tov}</td>
               <button
-                      name={player.name}
-                      className="player-add-button"
-                      id={player.id} 
-                      label={player}
-                      onClick={handleClick}>
-                        add
+                name={player.name}
+                className="player-add-button"
+                id={player.id}
+                label={player}
+                onClick={handleClick}
+              >
+                add
               </button>
-          </tr>
-            ))
-          }
-          </tbody>
-          </Table>
-        </div>
-      
-    )
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
-
-export default NbaTeam
+export default NbaTeam;
--- src/components/NbaTeamsContainer.jsx
+++ src/components/NbaTeamsContainer.jsx
@@ -1,37 +1,56 @@
-import { useSelector } from 'react-redux'
-import { selectAllTeams } from './redux/teamSlice'
-import { useNavigate } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
+import { useSelector } from "react-redux";
+import { selectAllTeams } from "./redux/teamSlice";
+import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
 
+function NbaTeamsContainer() {
+  const [isDesktop, setIsDesktop] = useState(true);
+  const teams = useSelector(selectAllTeams);
+  const teamsSlice = teams[0]?.slice(0, 30);
+  const navigate = useNavigate();
 
-function NbaTeamsContainer(){
+  if (Screen.width > 500) {
+    setIsDesktop(true);
+  } else if (Screen.width < 500) {
+    setIsDesktop(false);
+  }
 
-    const teams = useSelector(selectAllTeams)
-    const teamsSlice = teams[0]?.slice(0, 30)
-    const navigate = useNavigate()
-
-    return(
-        <>
+  return (
+    <>
+      {isDesktop ? (
         <Container className="teams-container">
-        {teamsSlice?.map(team=> 
-        <row 
-        md={1}
-        className="justify-content-md-center"
-        key={team.id}>
-            <button className="team-button"
-                    onClick={()=> navigate(`/nba-teams/${team.id}`)}
-            >
-            <p className="team-link">
-               <strong>
-                {team.full_name}
-                </strong>
-            </p>
-            </button>
-            </row>)}
+          {teamsSlice?.map((team) => (
+            <row md={1} className="justify-content-md-center" key={team.id}>
+              <button
+                className="team-button"
+                onClick={() => navigate(`/nba-teams/${team.id}`)}
+              >
+                <p className="team-link">
+                  <strong>{team.full_name}</strong>
+                </p>
+              </button>
+            </row>
+          ))}
         </Container>
+      ) : (
+        <>
+          {teamsSlice?.map((team) => (
+            <div key={team.id}>
+              <button
+                className="team-button"
+                onClick={() => navigate(`/nba-teams/${team.id}`)}
+              >
+                <p className="team-link">
+                  <strong>{team.full_name}</strong>
+                </p>
+              </button>
+            </div>
+          ))}
         </>
-    )
+      )}
+    </>
+  );
 }
 
-
-export default NbaTeamsContainer
+export default NbaTeamsContainer;
